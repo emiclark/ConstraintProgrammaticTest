89ead801e5170138629b9b3f9401be1dd97a6a81
Emi Clark <emiko.clark@gmail.com>
2018-04-13 21:56:44 -0400

    refactor

--- ConstraintsProgrammaticTest/ViewController.swift
+++ ConstraintsProgrammaticTest/ViewController.swift
@@ -9,8 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK:- Properties
     let squareSide: CGFloat = 100
     var sq1 = Square()
+    
     let titleLabel: UILabel = {
        let label = UILabel()
         label.text = "Animations"
@@ -20,25 +23,24 @@ class ViewController: UIViewController {
         return label
     }()
     
+    // MARK:- View Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // add views
+        setupAllViews()
+    }
+    
+    func setupAllViews() {
+        // add Title
         view.addSubview(titleLabel)
         constrainTitleLabel()
         
+        // add sq1
         sq1.square.addSubview(sq1.label)
         view.addSubview(sq1.square)
         setupConstraintsSq1(square: sq1)
     }
     
-    func constrainTitleLabel() {
-        NSLayoutConstraint.activate([
-            titleLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 60),
-            titleLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
-        ])
-    }
-    
+    // MARK:- sq1 Methods
     @objc func handleTapForSq1(_ sender: UITapGestureRecognizer) {
         // reset square
         sq1.square.layer.cornerRadius = 0
@@ -52,25 +54,25 @@ class ViewController: UIViewController {
         sq1.square.addGestureRecognizer(tap)
         sq1.square.isUserInteractionEnabled = true
         
-         NSLayoutConstraint.activate([
+        NSLayoutConstraint.activate([
             sq1.square.topAnchor.constraint(equalTo: self.titleLabel.bottomAnchor, constant: 50),
             sq1.square.widthAnchor.constraint(equalToConstant: squareSide),
             sq1.square.heightAnchor.constraint(equalToConstant: squareSide),
             sq1.square.centerXAnchor.constraint(equalTo: view.centerXAnchor)
-        ])
+            ])
         
         NSLayoutConstraint.activate([
             square.label.widthAnchor.constraint(equalToConstant: 100),
             square.label.centerYAnchor.constraint(equalTo: square.square.centerYAnchor),
             square.label.centerXAnchor.constraint(equalTo: square.square.centerXAnchor)
-        ])
+            ])
     }
-
+    
     func animateSq1() {
         // transforms sq1 from a green square to a cyan circle upon clicking
-        UIView.animateKeyframes(withDuration: 0.5, delay: 0.25, options: [.calculationModePaced], animations: {
+        UIView.animateKeyframes(withDuration: 0.5, delay: 0.1, options: [.calculationModePaced], animations: {
             // Add animations
-
+            
             UIView.addKeyframe(withRelativeStartTime: 0.0/0.0, relativeDuration: 0.0/0.25, animations: {
                 self.sq1.square.transform = CGAffineTransform.identity.scaledBy(x: 1.25, y: 1.25)
             })
@@ -82,11 +84,19 @@ class ViewController: UIViewController {
             UIView.addKeyframe(withRelativeStartTime: 0.0/0.25, relativeDuration: 0.0/0.25, animations: {
                 self.sq1.square.layer.cornerRadius = self.squareSide/2
             })
-
+            
             UIView.addKeyframe(withRelativeStartTime: 0.5/0.5, relativeDuration: 0.5/0.5, animations: {
                 self.sq1.square.backgroundColor = .cyan
             })
         })
+    }
+    
+    // MARK:- Helper Methods
+    func constrainTitleLabel() {
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 60),
+            titleLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
+            ])
     }
 }
 
